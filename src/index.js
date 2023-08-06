b5ed6d36edea2ac0115c0a6e7629d9e1dad67fd5
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2023-08-06 23:35:32 +0100

    updated redux path in app.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "../../my-app/src/reducer";
+import store from "/Users/IK/Desktop/web development/frontendmentor.io challenges/kanbanApp/my-app/src/reducer";
 import { AuthProvider } from "./components/context/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
